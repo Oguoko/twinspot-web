8baa4150620b943d90d108ca879bbded94474858
Kevin Oguoko <kevoguoko@gmail.com>
2025-12-31 14:08:50 +0000

    Temporarily disable admin image upload to unblock build

--- twinspot-web/app/admin/images/actions.ts
+++ twinspot-web/app/admin/images/actions.ts
@@ -1,26 +1,11 @@
 "use server";
 
-import { adminStorage } from "@/lib/firebase/admin";
-import { saveImage } from "@/lib/data/images";
+/**
+ * TEMPORARILY DISABLED
+ * Firebase admin storage is not required for public site deployment.
+ * This file will be re-enabled once admin tooling is finalized.
+ */
 
-export async function uploadImage(formData: FormData) {
-  const file = formData.get("file") as File;
-  if (!file) throw new Error("No file uploaded");
-
-  const buffer = Buffer.from(await file.arrayBuffer());
-  const fileName = `${Date.now()}-${file.name}`;
-
-  const blob = adminStorage.file(`images/${fileName}`);
-
-  await blob.save(buffer, {
-    contentType: file.type,
-    public: true,
-  });
-
-  const url = `https://storage.googleapis.com/${adminStorage.name}/images/${fileName}`;
-
-  await saveImage({
-    url,
-    name: file.name,
-  });
+export async function uploadImage() {
+  throw new Error("Image upload temporarily disabled.");
 }
